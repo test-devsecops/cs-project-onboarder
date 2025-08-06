667224c138f08c6d80eb8a8f86db3a016b3d56e7
jeielaraneta <johnjeiel_araneta@SGKP-01611L.ensigninfosecurity.com>
2025-08-06 10:03:23 +0800

    adjusted sleep time

--- checkmarx_verify_default_protected_branch.py
+++ checkmarx_verify_default_protected_branch.py
@@ -27,8 +27,8 @@ def main():
     
     # Batch configuration
     batch_size = 100
-    batch_timeout = 30  # seconds
-    sleep_time = 2 # seconds
+    batch_timeout = 10  # seconds
+    sleep_time = 1 # seconds
 
     # Tracking counters
     repo_updated_count = 0
